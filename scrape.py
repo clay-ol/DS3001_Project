abb11724025448d555e930ccf624ced799bf422a
hcaouette <huntercaouette@gmail.com>
2020-04-27 13:38:52 -0400

    spotifycharts.com scraper implemented. attached Jan '17

---
+++ scrape.py
@@ -0,0 +1,17 @@
+import ssl
+import csv
+import requests
+import os
+
+filename= os.path.abspath(os.path.dirname(__file__))
+
+with open('dates.csv', newline='') as csvfile:
+    dates = csv.reader(csvfile, delimiter=' ')
+    for row in dates:
+        date=row[0]
+        # date='2017-01-01'
+        url = "https://spotifycharts.com/regional/global/daily/%s/download" % (date)
+        writeFile=filename+ + '/chart_scrapes/%s/.csv' % (date)
+        r=requests.get(url,allow_redirects=True)
+        with open(writeFile, 'wb') as f:
+            f.write(r.content)
