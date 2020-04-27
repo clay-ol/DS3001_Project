a5050ed2adff03d1716513d404162db1adb050f9
hcaouette <huntercaouette@gmail.com>
2020-04-27 10:49:23 -0400

    added oauth IDs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-""" import spotipy
+import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+SPOTIPY_CLIENT_ID = bf02bcb1126f4363b3a4a057c623d182
+SPOTIPY_CLIENT_SECRET = 6c563e24a4ff4700a2e25adc0a662d80
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
@@ -12,4 +14,4 @@ while results['next']:
     albums.extend(results['items'])
 
 for album in albums:
-    print(album['name']) """
+    print(album['name'])
